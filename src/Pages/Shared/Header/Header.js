4a69153535b2e7c1db2386323a4c03a1046eda86
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-11-28 14:00:24 +0600

    added some update

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,27 +1,24 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import LeftSideNav from "../LeftSideNav/LeftSideNav";
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Dragon News</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#features">All News</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
+              <NavDropdown
+                title="Categories"
+                id="collasible-nav-dropdown"
+                className="d-lg-none"
+              >
+                <NavDropdown.Item>
+                  <LeftSideNav />{" "}
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
