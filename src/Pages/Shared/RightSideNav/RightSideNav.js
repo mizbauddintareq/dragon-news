341114f555eb005ce112df96f8181d84b6bf3dbd
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-11-28 13:25:39 +0600

    added right side content

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,7 +1,42 @@
+import { Button, ButtonGroup, ListGroup } from "react-bootstrap";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaTwitch,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
 const RightSideNav = () => {
   return (
     <div>
-      <p>This is right side nav</p>
+      <ButtonGroup vertical>
+        <Button variant="outline-primary" className="px-5">
+          <FaGoogle /> Login with google
+        </Button>
+        <Button variant="outline-dark" className="mt-3 px-5">
+          <FaGithub /> Login with github
+        </Button>
+      </ButtonGroup>
+
+      <div className="mt-5">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="my-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaWhatsapp /> Whatsapp{" "}
+          </ListGroup.Item>
+          <ListGroup.Item className="my-2">
+            <FaTwitter /> Twitter{" "}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitch /> Twitch{" "}
+          </ListGroup.Item>
+          <ListGroup.Item className="my-2">Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
